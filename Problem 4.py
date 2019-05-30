bae179cd7b4026c89fabf99d9659436483e49ec2
Garth <garth.t.slaney@gmail.com>
2019-05-30 13:59:22 -0600

    Add

--- Problem 4.py
+++ Problem 4.py
@@ -1,11 +1,19 @@
 a = 1
 b = 1
-lp = 0
 
-while a < 100:
-    while b < 100:
-        b = b+1
+while a < 3:
+    while b < 3:
         x = a*b
-
+        k = x
+        z = 0
+        n = 0
+        while x != 0:
+            y = x % 10
+            x = x - y
+            z = z + (y * (pow(10, n)))
+            n = n + 1
+        if z == k:
+            print(z)
+        b = b + 1
     b = 1
     a = a+1
