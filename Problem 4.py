4a3fc53dbdc1dcc3c4b32abfee1dd595ac04bfdf
Garth <garth.t.slaney@gmail.com>
2019-06-05 14:01:35 -0600

    Add

--- Problem 4.py
+++ Problem 4.py
@@ -1,19 +1,21 @@
-a = 1
-b = 1
+a = 9
+b = 9
 
-while a < 3:
-    while b < 3:
+while a < 10:
+    while b < 10:
         x = a*b
         k = x
         z = 0
         n = 0
         while x != 0:
-            y = x % 10
+            y = x % (pow(10, (n+1)))
             x = x - y
-            z = z + (y * (pow(10, n)))
+            z = z + y * pow(10, (2-n))
             n = n + 1
         if z == k:
             print(z)
+        else:
+            print(z, "a")
         b = b + 1
     b = 1
     a = a+1
