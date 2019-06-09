8389766a1e151907b518ac4de1ae8a44ff5e0aa6
Garth <garth.t.slaney@gmail.com>
2019-06-09 17:07:06 -0600

    Add

--- Problem 4.py
+++ Problem 4.py
@@ -1,21 +1,33 @@
-a = 9
-b = 9
+a = 1
+b = 1
+total = 0
 
-while a < 10:
-    while b < 10:
+
+while a < 1000:
+    while b < 1000:
         x = a*b
         k = x
+        f = x
+        c = 0
+        n = 1
         z = 0
-        n = 0
         while x != 0:
-            y = x % (pow(10, (n+1)))
-            x = x - y
-            z = z + y * pow(10, (2-n))
-            n = n + 1
-        if z == k:
+            x = int(x/10)
+            c = c + 1
+        while k != 0:
+            y = k % 10
+            k = k - y
+            z = z + (y * (10 ** (c-n)))
             print(z)
+            k = round(k/10)
+            n = n + 1
+        if z == f:
+            print(z)
+            if(z>total):
+                total = z
         else:
             print(z, "a")
         b = b + 1
     b = 1
-    a = a+1
+    a = a + 1
+print(total)
