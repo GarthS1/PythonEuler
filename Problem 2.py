fb11bab472509b7ba55667c86bafd1aa9480669c
Garth <garth.t.slaney@gmail.com>
2019-06-13 15:35:06 -0600

    Add

--- Problem 2.py
+++ Problem 2.py
@@ -1,13 +1,12 @@
-f1 = 1
-f2 = 2
-sum = 0
+f1 = 1  # first term in Fibonacci sequence
+f2 = 2  # second term in Fibonacci sequence
+total = 0
 
-while(f2<=4000000):
-    if(f2%2==0):
-        sum += f2
-    temp = f2
-    f2 += f1
-    f1 = temp
+while f2 <= 4000000:  # stops from going over 4 million
+    if f2 % 2 == 0:  # checks if even-valued
+        total += f2
+    temp = f2  # store the second term to a temporary term
+    f2 += f1  # adds the second and first term together to get your new second term
+    f1 = temp  # replaces the first term with second term
 
-print(sum)
-
+print(total)
