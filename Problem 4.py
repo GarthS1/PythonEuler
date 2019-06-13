ed714554d4042edd411a5e682bfbc771b0a82068
Garth <garth.t.slaney@gmail.com>
2019-06-13 15:50:06 -0600

    Add

--- Problem 4.py
+++ Problem 4.py
@@ -1,29 +1,29 @@
 a = 1
 b = 1
-total = 0
+total = 0  # stores the largest palindrome
 
 
-while a < 1000:
-    while b < 1000:
-        x = a*b
-        k = x
+while a < 1000:  # keeps first number in range of 1000
+    while b < 1000:   # keeps second number in range of 1000
+        x = a*b  # calculates product
+        k = x  # stores x in k and f for future use
         f = x
-        c = 0
-        n = 1
-        z = 0
-        while x != 0:
+        c = 0  # counter for how many digits a number has
+        n = 1  # counter for how many times the palindrome loop has run
+        z = 0  # stores the palindrome number
+        while x != 0:  # calculates the amount of digits in x
             x = int(x/10)
             c = c + 1
-        while k != 0:
-            y = k % 10
-            k = k - y
-            z = z + (y * (10 ** (c-n)))
-            k = round(k/10)
+        while k != 0:  # continues until k is zero
+            y = k % 10  # y is the number for the last digit in x
+            k = k - y  # removes the last digit to 0
+            z = z + (y * (10 ** (c-n)))  # puts the number in the opposite digit slot
+            k = round(k/10)  # reduces the digits by 1 for future use
             n = n + 1
-        if z == f:
-            if z > total:
+        if z == f:  # checks if palindrome
+            if z > total:  # checks if larger palindrome then previous one saved
                 total = z
-        b = b + 1
-    b = 1
+        b = b + 1  # increase by 1
+    b = 1  # reset to 1
     a = a + 1
 print(total)
