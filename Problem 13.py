849c5cd22d2901c10b037bd8c40c86bed0e137c4
Garth <garth.t.slaney@gmail.com>
2019-07-03 19:26:58 -0600

    Add

--- Problem 13.py
+++ Problem 13.py
@@ -1,2 +1,9 @@
 # Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 
+k = open("sum.txt", 'r')  # open the file needed
+theInts = []  # creates an empty list for storage
+for val in k.read().split():  # creates a list for the ints
+    theInts.append(int(val))
+k.close()
+
+print(sum(theInts))  # prints the sum of the list
