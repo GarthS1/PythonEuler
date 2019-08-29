ea9c97cf036eedff9730d4643e1343ab1e635bda
Garth <garth.t.slaney@gmail.com>
2019-08-28 20:07:27 -0600

    Add

---
+++ Problem 16.py
@@ -0,0 +1,14 @@
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# What is the sum of the digits of the number 21000?
+
+
+sumx = 0
+x = pow(2, 1000)
+
+while x > 0:
+    sumx += int(x % 10)
+    x = int(x - (x % 10))
+    x = int(x / 10)
+
+print(sumx)
+print(x)
